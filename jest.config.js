813bd12b9e62f12efb77f3f7650ef238af5a8b4c
kimseunggyu <dev.seunggyu@gmail.com>
2023-06-04 16:48:57 +0900

    chore: jest 설정 변경 및 Readme 변경

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
+  verbose: true, // verbose 는 테스트 수행시 테스트 내용 확인
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/index.{ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/index.{ts,tsx}',
+    '!src/**/*.d.{ts,tsx}',
+  ],
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '/src/serviceWorker.ts',
