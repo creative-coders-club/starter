8e022ad2c85f18e19ebec563423e3c1ab0126a91
정성준 <jvn4develop@gmail.com>
2023-06-11 17:12:36 +0900

    Todo 페이지 스타일 수정

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,9 +1,10 @@
 import { Todo } from './Todo';
 import { TodoType } from '../../types';
+import S from './TodoPage.module.scss';
 
 export const TodoList = ({ todos, removeTodo }: TodoListProps) => {
   return (
-    <ul>
+    <ul className={S.todo_list}>
       {todos.map((todo) => (
         <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
       ))}
